aa4d348b50e76a43317c67366d9123084cf09141
Hoang Hieu <hoanghieu.hcmut.1999@gmail.com>
2020-11-28 16:33:21 +0700

    change config

--- src/config/momo_payment.js
+++ src/config/momo_payment.js
@@ -1,7 +1,7 @@
 export const momoConfig = {
     initPaymentEndpoint: 'https://test-payment.momo.vn/gw_payment/transactionProcessor',
     partnerCode : 'MOMOIQA420180417',
-    accessKey: 'Q8gbQHeDesB2Xs0t',
+    accessKey: 'SvDmj2cOTYZmQQ3H',
     secretKey: 'PPuDXq1KowPT1ftR8DvlQTHhC03aul17',
     requestType: "captureMoMoWallet",
     notifyUrl: "https://payment-server-momo.herokuapp.com/ipn-payment", // need to change
